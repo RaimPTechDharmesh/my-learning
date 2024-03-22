e314d93c18f0db02e2462b1221db5ce1777bf13d
RaimPTechDharmesh <dharmeshraimp@gmail.com>
2024-03-22 17:16:15 +0530

    Refactor code add css and remove unused code.

--- manage-state/weekly-task/src/App.js
+++ manage-state/weekly-task/src/App.js
@@ -3,73 +3,18 @@ import {useEffect, useState} from "react";
 import {Button, Checkbox, Spin, Typography} from 'antd';
 import DataTable from "./component/DataTable";
 const { Title } = Typography;
-// import {createStandaloneToast} from "@chakra-ui/toast";
-
-// const { toast } = createStandaloneToast();
 
 function App() {
-  // const [isLoading, setIsLoading] = useState(false);
-  //
-  // // useEffect(() => {
-  // //   manageStateData();
-  // // }, []);
-  //
-  // const manageStateData = async () => {
-  //   try {
-  //     await new Promise((resolve) => setTimeout(resolve, 4000));
-  //
-  //     setIsLoading(true);
-  //
-  //     await new Promise((resolve) => setTimeout(resolve, 4000));
-  //
-  //     // For demonstration purposes, throwing an error to simulate API failure
-  //     throw new Error('Parameter is not a number!');
-  //   } catch (e) {
-  //     console.error(e); // Temporary log for error
-  //     // Code to show the error here
-  //   } finally {
-  //     // Set loading state back to false regardless of success or failure
-  //     setIsLoading(false);
-  //   }
-  // };
 
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    const [transactionOpen, setTransactionOpen] = useState(false);
-    const [isTransactionDataLoading, setIsTransactionDataLoading] =
+    const [informationOpen, setInformationOpen] = useState(false);
+    const [isDeveloperDataLoading, setIsDeveloperDataLoading] =
         useState(true);
 
     const [clicked, setClicked] = useState(true);
-
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-    //             setIsTransactionDataLoading(true);
-    //             const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-    //             if (!response.ok) {
-    //                 throw new Error('Failed to fetch data');
-    //             }
-    //             const json = await response.json();
-    //             setData(json);
-    //         } catch (error) {
-    //             setError(error);
-    //         } finally {
-    //             setIsLoading(false);
-    //             setIsTransactionDataLoading(false);
-    //         }
-    //     };
-    //
-    //     fetchData();
-    // }, []);
-
-    // if (isLoading) {
-    //     return <div>Loading...</div>;
-    // }
-    // if (error) {
-    //     return <div>Error: {error.message}</div>;
-    // }
 
     const handleClick = () => {
         setClicked(!clicked);
@@ -79,6 +24,7 @@ function App() {
     const fetchData = async () => {
         setIsLoading(true);
         try {
+            setIsDeveloperDataLoading(true);
             await new Promise((resolve) => setTimeout(resolve, 4000));
             const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
             if (!response.ok) {
@@ -90,6 +36,7 @@ function App() {
             setError(error);
         } finally {
             setIsLoading(false);
+            setIsDeveloperDataLoading(false);
         }
     };
 
@@ -102,24 +49,24 @@ function App() {
 
     const handleInvoiceCheckbox = (value) => {
         if (value) {
-            setTransactionOpen(true);
-            setIsTransactionDataLoading(false);
+            setInformationOpen(true);
+            setIsDeveloperDataLoading(false);
         } else {
-            setTransactionOpen(false);
+            setInformationOpen(false);
         }
     };
 
   return (
       <div className="App">
-          <h1>I Manage State For using Antd library.</h1>
-          {/*<div>*/}
-          {/*    <h1>Data from API:</h1>*/}
-          {/*    <pre>{JSON.stringify(data, null, 2)}</pre>*/}
-          {/*</div>*/}
+          <h1 style={{fontFamily: 'serif'}}>I manage state for using ReactJs, Antd library.</h1>
 
-          <div>
-              <Button type="primary" onClick={handleClickCallAPI}>Call API</Button>
-              {isLoading && <div>Loading...</div>}
+          <div className={'api-component'}>
+              <Button className="button" type="primary" onClick={handleClickCallAPI}>Call API</Button>
+                  {isDeveloperDataLoading && (
+                  <Spin tip="Loading" size="large" alignItems={'center'}/>
+              )}
+
+              {isLoading && <div style={{ padding: '10px'}}>Loading...</div>}
               {error && <div>Error: {error.message}</div>}
               {data && (
                   <div>
@@ -133,17 +80,13 @@ function App() {
           <div className='component'>
               <Checkbox
                   size="md"
-                  isDisabled={isTransactionDataLoading}
                   onChange={(event) => {
                       handleInvoiceCheckbox(event.target.checked);
                   }}
               >
-                  Include Transaction Fees
+                  Include Developer Information
               </Checkbox>
-              {/*{isTransactionDataLoading && (*/}
-              <Spin size="small" alignItems={'center'}/>
-              {/*)}*/}
-              {transactionOpen && (
+              {informationOpen && (
                   <>
                       <Title
                           fontWeight={700}
@@ -152,7 +95,7 @@ function App() {
                           pl={'10px'}
                           m={2}
                       >
-                          Available Transaction Fees
+                          Available Developer Information
                       </Title>
                       <DataTable/>
                   </>
