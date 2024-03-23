cb837998f1fdbfe758020f313c739b9ee664d05f
RaimPTechDharmesh <dharmeshraimp@gmail.com>
2024-03-23 11:55:20 +0530

    Set the dynamo db coding structure.

--- express/sample-express-dynamo-db/index.js
+++ express/sample-express-dynamo-db/index.js
@@ -1,27 +1,52 @@
 const express = require('express')
-const {Schema} = require('mongoose');
-const mongoose = require('mongoose');
 const app = express()
 const port = 3000
 
 app.use(express.json())
 
-mongoose.connect('mongodb://127.0.0.1:27017/localDb');
-const UserModel = mongoose.model('users', new Schema({ userName: String, password:String }));
+const AWS = require('aws-sdk');
+
+// Replace with your actual AWS region
+const region = 'us-east-2';
+
+// Configure DynamoDB with environment variables (assuming they are set)
+AWS.config.update({
+    region,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+console.log(process.env.AWS_ACCESS_KEY_ID);
+console.log(process.env.AWS_SECRET_ACCESS_KEY);
+
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const tableName = 'Dharmesh-User';
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.post('/', async (req, res) => {
+    const { userName, password } = req.body;
+    const params = {
+        TableName: tableName,
+        Key: {
+            userName
+        }
+    };
 
-    const response = await UserModel.findOne({ userName: req.body.userName, password: req.body.password });
 
-
-    if(response){
-        res.send("Login Successful")
-    } else {
-        res.status(401);
-        res.send("Login Failed")
+    try {
+        const data = await docClient.get(params).promise();
+        if (data.Item?.userName === password) {
+            res.json('log in successful');
+        } else {
+            res.status(401).send('log in failed');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
 });
 
