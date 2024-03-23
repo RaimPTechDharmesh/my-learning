faab2bbc5e655c5ba4238b8395d6ebda7ee764c7
RaimPTechDharmesh <dharmeshraimp@gmail.com>
2024-03-23 11:29:33 +0530

    Changes for the unused code.

--- express/sample-express-mongoose/index.js
+++ express/sample-express-mongoose/index.js
@@ -9,30 +9,20 @@ app.use(express.json())
 mongoose.connect('mongodb://127.0.0.1:27017/localDb');
 const UserModel = mongoose.model('users', new Schema({ userName: String, password:String }));
 
-const AWS = require('aws-sdk');
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.post('/', async (req, res) => {
 
-AWS.config.update({
-    region: 'us-east-2', // e.g., 'us-east-1'
-    accessKeyId: 'your-access-key-id',
-    secretAccessKey: 'your-secret-access-key'
+    const response = await UserModel.findOne({ userName: req.body.userName, password: req.body.password });
+
+    if(response){
+        res.send("Login Successful")
+    } else {
+        res.status(401);
+        res.send("Login Failed")
+    }
 });
-
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
-// app.post('/', async (req, res) => {
-//
-//     const response = await UserModel.findOne({ userName: req.body.userName, password: req.body.password });
-//
-//
-//     if(response){
-//         res.send("Login Successful")
-//     } else {
-//         res.status(401);
-//         res.send("Login Failed")
-//     }
-// });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
